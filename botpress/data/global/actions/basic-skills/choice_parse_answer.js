f1092f41dbcf8f419c8c24c0d58383bc77f288e1
Son Nguyen <son@bachmi.com>
2020-08-22 19:47:33 +0700

    Initial commit

---
+++ botpress/data/global/actions/basic-skills/choice_parse_answer.js
@@ -0,0 +1,56 @@
+//CHECKSUM:d9f5edd1114742d2a4bdbd53ce6de3ac2e0f5672d727ff09a9b65e56f145152b
+'use strict'
+const _ = require('lodash')
+const INTENT_PREFIX = 'intent:'
+
+/**
+ * Get a variable under this user's storage
+ * @title Validate user choice
+ * @category Skills
+ * @hidden true
+ * @author Botpress, Inc.
+ * @param {string} data - The parameters of the available choices
+ */
+const validateChoice = async data => {
+  let choice = undefined
+  const config = await bp.config.getModuleConfigForBot('basic-skills', event.botId)
+  const nb = _.get(event.preview.match(/^[#).!]?([\d]{1,2})[#).!]?$/), '[1]')
+
+  if (config.matchNumbers && nb) {
+    const index = parseInt(nb) - 1
+    const element = await bp.cms.getContentElement(event.botId, data.contentId)
+    choice = _.get(element, `formData.choices.${index}.value`)
+  }
+
+  if (!choice && config.matchNLU) {
+    choice = _.findKey(data.keywords, keywords => {
+      const intents = keywords
+        .filter(x => (x || '').toLowerCase().startsWith(INTENT_PREFIX))
+        .map(x => x.substr(INTENT_PREFIX.length))
+      return _.some(intents, k => event.nlu.intent.name === k)
+    })
+  }
+
+  if (!choice) {
+    const preview = (event.preview || '').toLowerCase()
+    const userText = ((event.payload && event.payload.text) || '').toLowerCase()
+    const choiceValue = ((event.payload && event.payload.payload) || '').toLowerCase()
+
+    choice = _.findKey(data.keywords, keywords =>
+      _.some(keywords || [], k => {
+        const keyword = (k || '').toLowerCase()
+        return preview.includes(keyword) || userText.includes(keyword) || choiceValue.includes(keyword)
+      })
+    )
+  }
+
+  const keySuffix = args.randomId ? `-${args.randomId}` : ''
+  if (choice) {
+    temp[`skill-choice-valid${keySuffix}`] = true
+    temp[`skill-choice-ret${keySuffix}`] = choice
+  } else {
+    temp[`skill-choice-valid${keySuffix}`] = false
+  }
+}
+
+return validateChoice(args)
