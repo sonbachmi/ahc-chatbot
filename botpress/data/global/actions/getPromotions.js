fb78c3fbc7af2b6dc3006490668657021dabe24c
Son Nguyen <son@bachmi.com>
2020-08-26 20:55:06 +0700

    lazada api

--- botpress/data/global/actions/getPromotions.js
+++ botpress/data/global/actions/getPromotions.js
@@ -1,16 +1,36 @@
-const axios = require('axios');
+// const axios = require('axios');
+//
+// // Fetch promotions
+// axios.get('/promotions')
+//     .then(function (response) {
+//         // handle success
+//         console.log(response);
+//         event.state.temp.promotions = response.promotions;
+//     })
+//     .catch(function (error) {
+//         // handle error
+//         console.log(error);
+//     })
+//     .then(function () {
+//         // always executed
+//     });
 
-// Fetch promotions
-axios.get('/promotions')
-    .then(function (response) {
-        // handle success
-        console.log(response);
-        event.state.temp.promotions = response.promotions;
-    })
-    .catch(function (error) {
-        // handle error
-        console.log(error);
-    })
-    .then(function () {
-        // always executed
+const LazadaAPI = require('lazada-open-platform-sdk');
+
+const getPromotions = async () => {
+
+// const lazada = new LazadaAPI('122307', 'M4Y4bm1WYwgGW714XoisrtzVMsDIbPEu', 'VIETNAM');
+    const lazada = new LazadaAPI('122309', 'sjcJVjql9uGKQys0XgE7AEgynMITsP9M', 'VIETNAM');
+
+    let response = await lazada.getBrands({
+        offset: '0',
+        limit: '10',
     });
+    const brands = response.data;
+    response = await lazada.getCategoryTree({
+    });
+    const categories = response.data;
+    temp = event.state.temp = {brands, categories};
+};
+
+return getPromotions();
